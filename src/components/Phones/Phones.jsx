77ea7c15ba53b814c711a7962ed2f5164875d74e
Nikhil Sarker <100969232+NikhilSarker@users.noreply.github.com>
2023-09-17 13:42:35 -0400

    loader part added

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,44 +1,60 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  
+  XAxis,
+  YAxis,
+  
+  Tooltip,
+} from "recharts";
 
-
+import { Audio } from "react-loader-spinner";
 const Phones = () => {
+  const [phone, setPhone] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [phone, setPhone]= useState([]);
+  useEffect(() => {
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const phoneData = data.data.data;
+        const phoneWithFakeData = phoneData.map((phone) => {
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("-")[1]),
+          };
+          return obj;
+        });
 
-  useEffect(()=>{
-    
-       axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-       .then(data =>{        
-          const phoneData = data.data.data;
-          const phoneWithFakeData = phoneData.map((phone)=>{
-            const obj = {
-              name: phone.phone_name,
-              price: parseInt(phone.slug.split('-')[1]),
-            
-            }
-            return obj;
-          })
-  
-         setPhone(phoneWithFakeData);
-       })
-      
-     
-
-  },[]);
-
-  
+        setPhone(phoneWithFakeData);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-       <BarChart width={1200} height={400} data={phone}>
-          <Bar dataKey="price" fill="#8884d8" />
-          <XAxis dataKey="name"></XAxis>
-          <YAxis></YAxis>
-          <Tooltip></Tooltip>
-        </BarChart>
       
+       { loading && 
+       <div>
+         <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="three-dots-loading"
+          wrapperStyle
+          wrapperClass
+         />
+       </div>}
+      
+      <BarChart width={1200} height={400} data={phone}>
+        <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey="name"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
+      </BarChart>
     </div>
   );
 };
